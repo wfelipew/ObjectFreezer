8c5664b97e707526eff851d607ff994871d54288
root <root@iron-trooper-laptop.(none)>
2012-04-01 16:48:06 -0300

    Teste de exceptions

--- tests/dummyTest.php
+++ tests/dummyTest.php
@@ -1,9 +1,21 @@
 <?php
-class StackTest extends PHPUnit_Framework_TestCase
+
+namespace  Freezer\tests\Dummy;
+
+class StackTest extends \PHPUnit_Framework_TestCase
 {
 	public function testDummy()
 	{
 		$this->assertEquals(3,3);
+	}
+
+
+	/**
+	*@expectedException InvalidArgumentException
+	*/
+	public function testDummyException()
+	{
+		throw new \InvalidArgumentException("Execao");
 	}
 }
 
